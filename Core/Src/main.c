531f6ce7783bc14341cb982e5c647854b0c8f8c0
Sohum Nigam <sohum.arjit.nigam@gmail.com>
2026-03-22 13:27:27 -0700

    Initial commit: basline working biphasic signal generation

---
+++ Core/Src/main.c
@@ -0,0 +1,362 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+volatile uint32_t phase_0;
+volatile uint32_t phase_1;
+volatile uint32_t phase_2;
+volatile uint32_t phase_3;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+static void TIM1_Init(void);
+static void GPIO_Init(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  TIM1_Init();
+  GPIO_Init();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+  {
+      RCC->CR |= RCC_CR_HSION;
+      while(!(RCC->CR & RCC_CR_HSIRDY));
+
+      RCC->PLLCFGR =
+          (16 << RCC_PLLCFGR_PLLM_Pos) |
+          (336 << RCC_PLLCFGR_PLLN_Pos) |
+          (1 << RCC_PLLCFGR_PLLP_Pos) |     // /4
+          RCC_PLLCFGR_PLLSRC_HSI |
+          (2 << RCC_PLLCFGR_PLLQ_Pos);
+
+      RCC->CR |= RCC_CR_PLLON;
+      while(!(RCC->CR & RCC_CR_PLLRDY));
+
+      FLASH->ACR |= FLASH_ACR_LATENCY_2WS;
+
+      RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+      RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+      while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
+  }
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+static void GPIO_Init(void){
+	//clock initializations
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+
+
+	//configre pin as alternate function
+	//PA7
+	GPIOA->MODER &= ~GPIO_MODER_MODER7_0;
+	GPIOA->MODER |= GPIO_MODER_MODER7_1;
+	//PA8
+	GPIOA->MODER &= ~GPIO_MODER_MODER8_0;
+	GPIOA->MODER |= GPIO_MODER_MODER8_1;
+
+	//Set output type as push-pull
+	GPIOA->OTYPER &= ~(GPIO_OTYPER_OT7|GPIO_OTYPER_OT8); //PA7 AND PA8
+
+	//no pull up or pull down
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPD7|GPIO_PUPDR_PUPD8);
+
+	//output speed
+	GPIOA->OSPEEDR |= GPIO_OSPEEDR_OSPEED7|GPIO_OSPEEDR_OSPEED8;
+
+	//ALTERNATE FUNCTION SELECTION
+	//AF1
+	GPIOA->AFR[0] |= GPIO_AFRL_AFRL7_0;
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFRL7_1;
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFRL7_2;
+	GPIOA->AFR[0] &= ~GPIO_AFRL_AFRL7_3;
+
+	GPIOA->AFR[1] |= GPIO_AFRH_AFRH0_0;
+	GPIOA->AFR[1] &= ~GPIO_AFRH_AFRH0_1;
+	GPIOA->AFR[1] &= ~GPIO_AFRH_AFRH0_2;
+	GPIOA->AFR[1] &= ~GPIO_AFRH_AFRH0_3;
+
+}
+
+  /* USER CODE END MX_GPIO_Init_2 */
+
+/* USER CODE BEGIN 4 */
+static void TIM1_Init(void){
+    __HAL_RCC_TIM1_CLK_ENABLE(); // Enable TIM1 clock
+
+
+
+
+
+    /*
+	uint32_t cycle_ticks = (84000000/(TIM1->PSC + 1)/(TIM1->ARR))/freq;
+
+	phase_0 = (cycle_ticks - 50)/2;
+	phase_1 = 25;
+	phase_2 = (cycle_ticks - 50)/2;
+	phase_3 = 25;
+     */
+
+
+    TIM1->PSC = 8399;       // Prescaler
+
+    uint8_t freq = 2;
+
+    uint32_t ARR_VAL  = (84000000 / (TIM1->PSC + 1) / freq) - 1;
+
+
+    //TIM1->CR1 |= TIM_CR1_ARPE;       //Auto reload preload
+    TIM1->ARR = ARR_VAL;      // Auto-reload
+    TIM1->CCR1 = ARR_VAL/2;
+
+    TIM1->EGR |= TIM_EGR_UG; // Generate an update event to load PSC/ARR
+
+    TIM1->CCMR1 &= ~TIM_CCMR1_OC1M_0; //Physical pin D7 on arduino headers set to output PWM
+    TIM1->CCMR1 |= TIM_CCMR1_OC1M_1;
+    TIM1->CCMR1 |= TIM_CCMR1_OC1M_2;
+
+    TIM1->CCMR1 |= TIM_CCMR1_OC1PE;
+
+    TIM1->CCER &= ~TIM_CCER_CC1NP;
+
+    TIM1->CCER |= TIM_CCER_CC1E; //Enable the output and its complementary
+    TIM1->CCER |= TIM_CCER_CC1NE;
+
+    //TIM1->BDTR |= TIM_BDTR_OSSI;
+
+	TIM1->BDTR |= TIM_BDTR_MOE;
+
+	TIM1->CR1 |= TIM_CR1_CEN;
+
+
+    //HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 0, 0);
+    //HAL_NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
+
+
+
+}
+/*
+uint8_t state = 0;
+
+uint32_t counter = 0;
+
+void TIM1_UP_TIM10_IRQHandler(void){
+    if (TIM1->SR & TIM_SR_UIF){
+        TIM1->SR &= ~TIM_SR_UIF;
+
+        counter += 1;
+
+        switch(state){
+
+        case 0:
+        	if(counter >= phase_0){
+				GPIOC->ODR &= ~GPIO_ODR_OD0;
+				GPIOC->ODR &= ~GPIO_ODR_OD1;
+        		state = 1;
+        		counter = 0;
+        	}
+        	break;
+
+        case 1:
+        	if(counter >= phase_1){
+				GPIOC->ODR |= GPIO_ODR_OD0;
+				GPIOC->ODR &= ~GPIO_ODR_OD1;
+        		state = 2;
+        		counter = 0;
+        	}
+
+        	break;
+        case 2:
+
+            if(counter >= phase_2){
+				GPIOC->ODR &= ~GPIO_ODR_OD0;
+				GPIOC->ODR &= ~GPIO_ODR_OD1;
+            	state = 3;
+          		counter = 0;
+            }
+            break;
+
+        case 3:
+
+            if(counter >= phase_3){
+				GPIOC->ODR |= GPIO_ODR_OD1;
+				GPIOC->ODR &= ~GPIO_ODR_OD0;
+        		state = 0;
+        		counter = 0;
+        	}
+            break;
+
+        }
+
+
+    }
+}
+*/
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
